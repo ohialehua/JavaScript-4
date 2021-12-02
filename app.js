93e543a033c639bdcaa8694f8123d2194aed770a
Kairyu <kairyu.bonvivant@gmail.com>
2021-12-02 13:50:56 +0900

    FINISH

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.box1').slideDown(function(){
       $('.box1').css({
-        'background-color': '#0000ff',
+        'background-color': '#0000FF',
         'height': '100px',
       })
       $('.box1').slideUp();
